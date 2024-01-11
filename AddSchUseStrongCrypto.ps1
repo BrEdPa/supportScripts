581b97f84ec35505c440ab5059560b48a63ea4a3
Starch Patriarch <144400608+BrEdPa@users.noreply.github.com>
2024-01-11 08:58:01 -0600

    Add files via upload
    The first powershell script I wrote, along with the script to make registry changes for a piece of software in my job's environment. Both of these were related to massive projects my team & I were responsible for. Not exactly automating the entire job, but saving some seconds and maybe some sanity.

---
+++ AddSchUseStrongCrypto.ps1
@@ -0,0 +1,14 @@
+﻿##BrEdPa, 2023
+##MiPACS dental software requires some registry changes in the environment to work correctly, this script makes those changes.
+
+#Set the variables
+$RegistryPath1 = 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319'
+$RegistryPath2 = 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319'
+$Name = 'SchUseStrongCrypto'
+$Value = '1'
+
+#create the registry entries
+New-ItemProperty -Path $RegistryPath1 -Name $Name -Value $Value -PropertyType DWORD -Force
+New-ItemProperty -Path $RegistryPath2 -Name $Name -Value $Value -PropertyType DWORD -Force
+
+
